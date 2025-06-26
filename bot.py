861776d82434e6a8caf6ad3f2def4d2708d720d8
Femxiao <femxiao@tuta.io>
2025-06-26 15:50:48 +0200

    Test

--- bot.py
+++ bot.py
@@ -1,5 +1,17 @@
-import random
 import discord
-import guilded
-import api
-import you know what sybau nigga I ain't doing shi
+
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print('Logged on as', self.user)
+
+    async def on_message(self, message):
+        if message.author == self.user:
+            return
+
+        if message.content == 'ping':
+            await message.channel.send('pong')
+
+intents = discord.Intents.default()
+intents.message_content = True
+client = MyClient(intents=intents)
+client.run('REPLACE WITH YOUR BOT TOKEN')
